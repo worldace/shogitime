1f217a802730c6025b9d5c1241745d7c6b0466f8
tokaiknight <tokaiknight@yahoo.co.jp>
2020-12-25 06:37:12 +0900

    no message

--- shogi-time.js
+++ shogi-time.js
@@ -249,8 +249,8 @@ class 将棋タイム extends HTMLElement{
 
 
 
-    $局面_click(event){
-        const {left, width} = this.$局面.getBoundingClientRect();
+    $将棋盤_click(event){
+        const {left, width} = this.$将棋盤.getBoundingClientRect();
         (event.clientX < left+width/2) ? this.$前に移動ボタン.click() : this.$次に移動ボタン.click()
     }
 
